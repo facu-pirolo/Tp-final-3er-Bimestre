f07020462aa349ec841277d317b050816eb0cfc4
facu-pirolo <49518308@est.ort.edu.ar>
2025-09-22 10:47:30 -0300

    dos puntos hechos

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -9,21 +9,34 @@ public class AgentScript : MonoBehaviour
     [SerializeField] Transform targetTR;
     [SerializeField] Animator anim;
     [SerializeField] float velocity;
+    [SerializeField] Transform [] puntosDePatru;
+    public bool Patrulaje = true;
+    int puntoActual = -1;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (puntosDePatru.Length > 0)
+        {
+            agent.destination = puntosDePatru[0].position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = targetTR.position;
+      if(!agent.pathPending && agent.remainingDistance < 0.3f && puntosDePatru.Length > 0)
+        { 
+            puntoActual = (puntoActual + 1) % puntosDePatru.Length;
+        agent.destination = puntosDePatru[puntoActual].position;
+    }
+
+    
         velocity = agent.velocity.magnitude;
         anim.SetFloat("Speed",velocity);
     }
