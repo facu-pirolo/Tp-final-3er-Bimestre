1655a219d800a3328b8a0e9812c9ae61aec0896b
facu-pirolo <49518308@est.ort.edu.ar>
2025-09-26 10:54:26 -0300

    ej 3 hecho

--- Assets/RayCats.cs
+++ Assets/RayCats.cs
@@ -1,39 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RayCats : MonoBehaviour
 {
+    NavMeshAgent agent;
     [SerializeField] Vector3 rayOrigin;
     [SerializeField] Transform sightOrigin;
-    [SerializeField] float rayDistance;
-    
+    [SerializeField] float rayDistance = 10f;
+    [SerializeField] Transform[] puntosDePatru;
+    [SerializeField] RayCats rayVision;
+    [SerializeField] bool Patrulaje = true;
+    int puntoActual = 0;
+    [SerializeField] Transform Jugador;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!agent.pathPending && agent.remainingDistance < 0.3f && puntosDePatru.Length > 0)
+        {
+            puntoActual = (puntoActual + 1) % puntosDePatru.Length;
+            agent.destination = puntosDePatru[puntoActual].position;
+        }
+
+        if (!Patrulaje)
+        {
+            agent.destination = Jugador.position;
+        }
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed", velocity);
+
         Ray ray = new Ray(sightOrigin.position, sightOrigin.forward);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, rayDistance))
         {
-            Debug.Log("Impactó con: " + hit.collider.gameObject.name);
-            if (hit.collider.gameObject.tag=="Jugador")
+            if (hit.collider.gameObject.tag == "Jugador")
             {
-
+                Jugador = hit.collider.transform;
+                Debug.Log("Detectó a: " + hit.collider.gameObject.name);
+                Patrulaje = false;
             }
         }
     }
 
+
     void OnDrawGizmos()
     {
+
         Color color = Color.red;
         Gizmos.color = color;
-        Gizmos.DrawLine(sightOrigin.position, sightOrigin.position + sightOrigin.forward * rayDistance); ;
+        Gizmos.DrawLine(sightOrigin.position, sightOrigin.position + sightOrigin.forward * rayDistance);
     }
 }
+
--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -9,9 +9,7 @@ public class AgentScript : MonoBehaviour
     [SerializeField] Transform targetTR;
     [SerializeField] Animator anim;
     [SerializeField] float velocity;
-    [SerializeField] Transform [] puntosDePatru;
-    public bool Patrulaje = true;
-    int puntoActual = -1;
+    
 
     private void Awake()
     {
@@ -21,23 +19,13 @@ public class AgentScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (puntosDePatru.Length > 0)
-        {
-            agent.destination = puntosDePatru[0].position;
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-      if(!agent.pathPending && agent.remainingDistance < 0.3f && puntosDePatru.Length > 0)
-        { 
-            puntoActual = (puntoActual + 1) % puntosDePatru.Length;
-        agent.destination = puntosDePatru[puntoActual].position;
-    }
-
-    
-        velocity = agent.velocity.magnitude;
+{
+      velocity = agent.velocity.magnitude;
         anim.SetFloat("Speed",velocity);
     }
 }
